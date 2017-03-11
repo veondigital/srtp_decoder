8b2b05002e83b91720b1fa83824b84ce7963ef2f
Aleksey Dorofeev <aleksei.dorofeev@steppechange.com>
2017-03-11 18:02:55 +0300

    add flag

--- tests/udp_call_02_03.cmd
+++ tests/udp_call_02_03.cmd
@@ -7,5 +7,5 @@ set SSTP_B=YTEzNjhhMTNmNWUxMjljZTg3MTEyNWQ2YTE2ODQ2
 
 set ALG=AES_CM_128_HMAC_SHA1_80
 
-..\win32_vs15\Debug\srtp_decoder.exe %PCAP_FILENAME%.pcap %PCAP_FILENAME%_A.opus %SSRC_A% %SSTP_A% %ALG% true 2>&1 >%PCAP_FILENAME%_A.log
-..\win32_vs15\Debug\srtp_decoder.exe %PCAP_FILENAME%.pcap %PCAP_FILENAME%_B.opus %SSRC_B% %SSTP_B% %ALG% true 2>&1 >%PCAP_FILENAME%_B.log
+..\win32_vs15\Debug\srtp_decoder.exe -l %PCAP_FILENAME%.pcap %PCAP_FILENAME%_A.opus %SSRC_A% %SSTP_A% %ALG% true 2>&1 >%PCAP_FILENAME%_A.log
+..\win32_vs15\Debug\srtp_decoder.exe -l %PCAP_FILENAME%.pcap %PCAP_FILENAME%_B.opus %SSRC_B% %SSTP_B% %ALG% true 2>&1 >%PCAP_FILENAME%_B.log
